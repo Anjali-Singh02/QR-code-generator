00667aca76eae199e1d5f74a2f1eef39b692a655
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-02 19:04:27 +0530

    modified theme object added color object

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				navColor: '#13315c',
+				bgDark: '#134074',
+				footerColor: '#0b2545',
+			},
+		},
+	},
+	plugins: [],
+};
