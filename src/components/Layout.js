6e6b6a4e8c4b3a29a285edf5d04e9c8ea99333ca
Anjali Singh <anjalikumarsingh015@gmail.com>
2023-05-04 00:39:45 +0530

    applied css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,12 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
-
 const Layout = ({ children }) => {
 	return (
 		<div className="flex flex-col w-full min-h-screen h-full ">
 			<Navbar />
-			<hr className="shadow-md border-fontColor" />
+			<hr className="drop-shadow-md border-2" />
 			<div className="flex-grow bg-bgDark">{children}</div>
-			<hr className="shadow-md border-fontColor" />
+			<hr className="drop-shadow-md border-2" />
 			<Footer />
 		</div>
 	);
